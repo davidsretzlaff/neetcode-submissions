85b1e3b00c477730f8c81fd93d3d8afa629f7b63
davidsretzlaff <davids.retzlaff@gmail.com>
2026-06-09 10:21:37 -0300

    Add: reverse-a-linked-list - submission-1

---
+++ Data Structures & Algorithms/reverse-a-linked-list/submission-1.cs
@@ -0,0 +1,23 @@
+
+ 
+public class Solution {
+    public ListNode ReverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+
+        while (curr != null)
+        {
+            var nextTemp = curr.next;
+            curr.next = prev;
+            //Console.WriteLine($"curr.next.val: {curr?.next?.val}");
+
+            prev = curr;
+            //Console.WriteLine($"prev.val: {prev?.val}");
+
+            curr = nextTemp;
+            //Console.WriteLine($"curr.val: {curr?.val}");
+
+        }
+        return prev;
+    }
+}
